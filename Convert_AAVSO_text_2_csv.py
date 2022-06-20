4326b1f98edde7840a244f5572e65b9a6c716e42
Jeffrey Rombough <101207131+romboughconsultingservices@users.noreply.github.com>
2022-06-20 13:09:52 -0500

    20220620
    20220620

---
+++ Convert_AAVSO_text_2_csv.py
@@ -0,0 +1,7 @@
+#Convert AAVSO text files to csv 
+#Author: Jeffrey C. Rombough
+#Company: RCSCS
+#20220620
+#Note the standard list of imported functions can be found at https://docs.python.org/3/library/
+
+import sys
---
+++ PythonDataToolsTemplate.py
@@ -0,0 +1,7 @@
+#Python Data Tools Template
+#Author: Jeffrey C. Rombough
+#Company: RCSCS
+#20220620
+#Note the standard list of imported functions can be found at https://docs.python.org/3/library/
+
+import sys
