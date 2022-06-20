9db33689dcef6860adcc3cafe7cc103030d59d81
Jeffrey Rombough <101207131+romboughconsultingservices@users.noreply.github.com>
2022-06-20 15:07:51 -0500

    202206201507

--- Convert_AAVSO_text_2_csv.py
+++ Convert_AAVSO_text_2_csv.py
@@ -4,4 +4,38 @@
 #20220620
 #Note the standard list of imported functions can be found at https://docs.python.org/3/library/
 
+#format of AAVSO files starts with 4 lines of garbage ASCII letters
+#This is followed by 14 numbers delimited with one or more spaces
+#There are 31 rows per file. Nulls can be counted at '0' 
+#The yearly means and other information after the 31st row is not needed.
+
 import sys
+
+source = sys.argv[1]
+destination = sys.argv[2]
+
+#Open source file
+sourceFile = open (source, 'r')
+#fileLineNumbers = enumerate(sourceFile.readlines())
+
+
+
+lineCounter = 0
+
+while lineCounter < 35:
+    line = sourceFile.readline()
+    print("Line{}: {}".format(lineCounter, line))
+    lineCounter += 1
+    # The above shows the only way to increment in python x++ won't work
+    # line number and content
+
+sourceFile.close()
+
+#destFile = 
+#Create and open destination csv file
+#Goto the 5th row
+# Copy Number from source to destination. Use newlines on both. This could be a for loop with 31 iterations
+# close both files
+
+
+
--- PythonDataToolsTemplate.py
+++ PythonDataToolsTemplate.py
@@ -5,3 +5,21 @@
 #Note the standard list of imported functions can be found at https://docs.python.org/3/library/
 
 import sys
+
+#Use argv for source and destination files names (or make them one and the same)
+print ("This is the name of the script: ", sys.argv[0])
+print ("Number of arguments: ", len(sys.argv))
+print ("The arguments are: " , str(sys.argv))
+source = sys.argv[1]
+destination = sys.argv[2]
+print(source + " as source")
+print(destination + " as destination")
+
+#C:\mx\rcscs\PythonDataTools>python Convert_AAVSO_text_2_csv.py arg1 arg2
+#This is the name of the script:  Convert_AAVSO_text_2_csv.py
+#Number of arguments:  3
+#The arguments are:  ['Convert_AAVSO_text_2_csv.py', 'arg1', 'arg2']
+
+#https://www.knowledgehut.com/blog/programming/sys-argv-python-examples
+
+
